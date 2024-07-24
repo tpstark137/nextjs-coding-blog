e03abfd0ae4d2a29013a0fac5afe10052b3d6fab
tpstark137 <tpaul.be21@gmail.com>
2024-07-25 01:28:43 +0530

    querying data with swr

--- src/components/home/popular-post.tsx
+++ src/components/home/popular-post.tsx
@@ -1,14 +1,20 @@
 "use client";
 
-import { popularPosts } from "@/lib/popularpost-data";
+//import { popularPosts } from "@/lib/popularpost-data";
+import { fetchUrl, fetcher } from "@/lib/utils";
 import { Icons } from "../icons";
 import Link from "next/link";
+import useSWR from "swr";
 
 export default function PopularPosts() {
+  const { data, error, isLoading } = useSWR(fetchUrl, fetcher);
+
+  if (error) return <div>Failed to load</div>;
+  if (isLoading) return <div>Loading...</div>;
   return (
     <ul className="overflow-auto">
-      {popularPosts?.map((post: { title: string }) => (
-        <Link href={`/blog/`} key={post.title}>
+      {data?.map((post: { category: string; slug: string; title: string }) => (
+        <Link href={`/blog/${post.category}/${post.slug}`} key={post.title}>
           <li className="flex items-center gap-2 group cursor-pointer py-2">
             <Icons.ArrowRight className="h-6 w-6 group-hover:translate-x-1 transition-all" />
             <p>{post.title}</p>
